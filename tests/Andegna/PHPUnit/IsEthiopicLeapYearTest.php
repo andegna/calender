d917013ae4417dcb8ba30a1b78d25ef0fbda3ad0
Sam As End <4sam21@gmail.com>
2015-02-02 14:02:31 +0300

    First commit

---
+++ tests/Andegna/PHPUnit/IsEthiopicLeapYearTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Andegna\PHPUnit;
+
+use \Andegna\Calender;
+
+
+class IsEthiopicLeapYearTest extends \PHPUnit_Framework_TestCase {
+
+    public function testMethodExists()
+    {
+//        $this->assertTrue(method_exists('Calender','isEthiopianLeapYear'));
+    }
+
+    public function methodDataProvider() {
+        return [
+            [-1 , true], // 1 A.D
+            [0 , false],
+            [1 , false],
+            [1998 , false],
+            [1999 , true],
+            [2000 , false],
+            [2001 , false],
+            [2002 , false],
+            [2003 , true],
+            [2004 , false],
+            [2005 , false],
+            [2006 , false],
+            [2007 , true],
+            [2008 , false]
+        ];
+    }
+
+    /**
+     * @dataProvider methodDataProvider
+     */
+    public function testMethod($year , $expected)
+    {
+        $this->assertEquals($expected, Calender::isEthiopianLeapYear($year));
+    }
+
+}
+ 
