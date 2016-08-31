c3ebce29a5e40b47b92360737d8b061cbc76dabd
Samson Endale <4sam21@gmail.com>
2016-08-31 09:35:36 +0000

    Applied fixes from StyleCI

--- tests/IsEthiopicLeapYearTest.php
+++ tests/IsEthiopicLeapYearTest.php
@@ -14,20 +14,20 @@ class IsEthiopicLeapYearTest extends \PHPUnit_Framework_TestCase
     public function methodDataProvider()
     {
         return [
-            [-1 , true], // 1 A.D
-            [0 , false],
-            [1 , false],
-            [1998 , false],
-            [1999 , true],
-            [2000 , false],
-            [2001 , false],
-            [2002 , false],
-            [2003 , true],
-            [2004 , false],
-            [2005 , false],
-            [2006 , false],
-            [2007 , true],
-            [2008 , false],
+            [-1, true], // 1 A.D
+            [0, false],
+            [1, false],
+            [1998, false],
+            [1999, true],
+            [2000, false],
+            [2001, false],
+            [2002, false],
+            [2003, true],
+            [2004, false],
+            [2005, false],
+            [2006, false],
+            [2007, true],
+            [2008, false],
         ];
     }
 
