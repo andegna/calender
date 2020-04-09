8b98495beafc16793425634139fe8ff2592bd129
Samson Endale <4sam21@gmail.com>
2020-04-09 23:25:31 +0300

    Implement Christmas holiday

---
+++ src/Holiday/Christmas.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Andegna\Holiday;
+
+use Andegna\DateTimeFactory;
+use Andegna\Validator\LeapYearValidator;
+use DateTimeZone;
+
+/**
+ * Ethiopian Christmas.
+ */
+class Christmas implements Holiday
+{
+    const TAHISAS = 4; // ታህሳስ ወር
+
+    /** @var DateTimeZone */
+    private $dateTimeZone;
+
+    /**
+     * Christmas constructor.
+     *
+     * @param DateTimeZone $dateTimeZone
+     */
+    public function __construct(DateTimeZone $dateTimeZone = null)
+    {
+        $this->dateTimeZone = $dateTimeZone;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get(int $year)
+    {
+        $day = 29;
+
+        if ($this->wasAfterALeapYear($year)) {
+            $day = 28;
+        }
+
+        return DateTimeFactory::of($year, self::TAHISAS, $day);
+    }
+
+    private function wasAfterALeapYear(int $year): bool
+    {
+        $previous = $year - 1;
+
+        return (new LeapYearValidator($previous))->isValid();
+    }
+
+}
---
+++ tests/Holiday/ChristmasTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Andegna\PHPUnit\Holiday;
+
+use Andegna\DateTime;
+use Andegna\Holiday\Christmas;
+use PHPUnit\Framework\TestCase;
+
+class ChristmasTest extends TestCase
+{
+    /** @var Christmas */
+    protected $christmas;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        date_default_timezone_set('Africa/Addis_Ababa');
+
+        $this->christmas = new Christmas(new \DateTimeZone('Africa/Addis_Ababa'));
+    }
+
+    /**
+     * Eastern Orthodox churches christmas dates.
+     *
+     * @return array
+     */
+    public function christmasDatesDataProvider()
+    {
+        return [
+            [2010, 29],
+            [2011, 29],
+            [2012, 28],
+            [2013, 29],
+            [2014, 29],
+            [2015, 29],
+            [2016, 28],
+            [2017, 29],
+        ];
+    }
+
+    /**
+     * @dataProvider christmasDatesDataProvider
+     *
+     * @param $year
+     * @param $day
+     */
+    public function test_christmas_date($year, $day)
+    {
+        $ethiopian = $this->christmas->get($year);
+
+        $this->assertEquals($year, $ethiopian->getYear());
+        $this->assertEquals(4, $ethiopian->getMonth());
+        $this->assertEquals($day, $ethiopian->getDay());
+    }
+}
