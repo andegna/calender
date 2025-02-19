e8deaa640a37dfe272add8451717bc60ea4631c0
Mintesnot Teshome <126505475+mintesnot-teshome@users.noreply.github.com>
2025-02-19 18:20:53 +0300

    formatted AgeCalculatorTest.php

--- tests/AgeCalculator/AgeCalculatorTest.php
+++ tests/AgeCalculator/AgeCalculatorTest.php
@@ -2,32 +2,32 @@
 
 namespace Andegna\Tests\AgeCalculator;
 
-use PHPUnit\Framework\TestCase;
 use Andegna\AgeCalculator\AgeCalculator;
 use Andegna\Exception\InvalidDateException;
+use PHPUnit\Framework\TestCase;
 
-
-    class AgeCalculatorTest extends TestCase
-    {
+class AgeCalculatorTest extends TestCase
+{
     public function testCalculateAgeReturnsInteger()
     {
   
-    $calculator = new AgeCalculator(1996,1, 21);
-    $age = $calculator->calculateAge();
-    $this->assertEquals($age, 21);
+        $calculator = new AgeCalculator(1996,1, 21);
+        $age = $calculator->calculateAge();
+        $this->assertEquals($age, 21);
     }
 
     public function testConstructorWithInvalidDateThrowsException()
     {
 
-    $this->expectException(InvalidDateException::class);
-    new AgeCalculator(2000, 14, 1);
+        $this->expectException(InvalidDateException::class);
+        new AgeCalculator(2000, 14, 1);
     }
 
     public function testCalculateAgeThrowsExceptionForFutureDate()
     {
-    $calculator = new AgeCalculator(3000, 1, 1);
-    $this->expectException(InvalidDateException::class);
-    $calculator->calculateAge();
+        $calculator = new AgeCalculator(3000, 1, 1);
+        $this->expectException(InvalidDateException::class);
+        $calculator->calculateAge();
     }
+    
     }
