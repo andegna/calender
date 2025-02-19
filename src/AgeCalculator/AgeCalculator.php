fefa446b97e36a7e101e90299bf167727f488c80
Mintesnot Teshome <mintesnotteshome0@gmail.com>
2025-02-19 18:33:53 +0300

    Refactor AgeCalculator and its tests for improved readability and consistency

--- src/AgeCalculator/AgeCalculator.php
+++ src/AgeCalculator/AgeCalculator.php
@@ -57,9 +57,10 @@ class AgeCalculator
     /**
      * Calculate the age based on the Ethiopian date.
      *
-     * @return int
      *
      * @throws \Andegna\Exception\InvalidDateException
+     *
+     * @return int
      */
     public function calculateAge(): int
     {
@@ -80,6 +81,8 @@ class AgeCalculator
         }
 
         $ageInterval = $currentDate->diff($gregorianDate);
+        
         return $ageInterval->y;
     }
+    
 }
